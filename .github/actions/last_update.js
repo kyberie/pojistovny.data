7267b80ab4f3a70afccc5efb69dfa222aceb1df5
Milan Somsedík <milan.somsedik@gmail.com>
2024-08-13 15:57:33 +0200

    v2

--- .github/actions/last_update.js
+++ .github/actions/last_update.js
@@ -59,6 +59,62 @@ fs.readFile('main/data.json', (err, data) => {
   archiveDomov.finalize();
 });
 
+fs.readFile('main/data_v2.json', (err, data) => {
+  let pojistovny = JSON.parse(data);
+  let zip_name_all = pojistovny.logos_zip_file_all;
+  let zip_name_automobil = pojistovny.logos_zip_file_automobil;
+  let zip_name_domov = pojistovny.logos_zip_file_domov;
+
+  var outputAll = fs.createWriteStream('main/' + zip_name_all);
+  var archiveAll = archiver('zip', {
+    zlib: { level: 9 } 
+  });
+  archiveAll.pipe(outputAll);
+
+  var outputAuto = fs.createWriteStream('main/' + zip_name_automobil);
+  var archiveAuto = archiver('zip', {
+    zlib: { level: 9 } 
+  });
+  archiveAuto.pipe(outputAuto);
+  
+  var outputDomov = fs.createWriteStream('main/' + zip_name_domov);
+  var archiveDomov = archiver('zip', {
+    zlib: { level: 9 } 
+  });
+  archiveDomov.pipe(outputDomov);
+
+  var logos = [];
+  var logosAuto = [];
+  var logosDomov = [];
+  for (index in pojistovny.insurers) {
+    var pojistovna = pojistovny.insurers[index];
+    if (logos[pojistovna.logo] != 1) {
+      archiveAll.file('main/logo/' + pojistovna.logo, { name: pojistovna.logo });
+      logos[pojistovna.logo] = 1;
+    }
+
+    if (pojistovna.app_automobil.use_in_app) {
+      if (logosAuto[pojistovna.logo] != 1) {
+        archiveAuto.file('main/logo/' + pojistovna.logo, { name: pojistovna.logo });
+        logosAuto[pojistovna.logo] = 1;
+      }
+    }
+	
+	if (pojistovna.app_domov.use_in_app) {
+      if (logosDomov[pojistovna.logo] != 1) {
+        archiveDomov.file('main/logo/' + pojistovna.logo, { name: pojistovna.logo });
+        logosDomov[pojistovna.logo] = 1;
+      }
+    }
+
+  //  console.log(pojistovna.logo);
+  }
+
+  archiveAll.finalize();
+  archiveAuto.finalize();
+  archiveDomov.finalize();
+});
+
 fs.writeFile('main/update.json', '{"update":"' + date.toISOString() + '"}', function (err) {
   if (err) throw err;
   console.log('Replaced!');
